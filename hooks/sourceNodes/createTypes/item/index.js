6487d8ef275fdfdd7bd7a8a912eb49a83a7726ba
federicobadini <federico@inkofpixel.com>
2021-08-31 22:46:59 +0200

    Update index.js

--- hooks/sourceNodes/createTypes/item/index.js
+++ hooks/sourceNodes/createTypes/item/index.js
@@ -132,7 +132,7 @@ module.exports = function (_ref) {
               valueNode: {
                 type: nodeType,
                 resolve: function resolve(node, args, context) {
-                  node.id = "".concat(_type, "-").concat(node.entityPayload.id, "-").concat(node.locale);
+                  node.id = "".concat(gqlItemTypeName(entity), "-").concat(node.entityPayload.id, "-").concat(node.locale);
                   var i18n = {
                     locale: node.locale,
                     fallbacks: localeFallbacks
